8f707046a2a4937512d0aafce2c84066d36913c1
anuarsabitovich <anuarkulmagambetov@gmail.com>
2024-04-01 18:44:25 +0500

    feat:  populate two models, configure DbContext, create migration and update-database

--- SynthShop/Data/MainDbContext.cs
+++ SynthShop/Data/MainDbContext.cs
@@ -1,6 +1,16 @@
-﻿namespace SynthShop.Data
+﻿using Microsoft.EntityFrameworkCore;
+using SynthShop.Data.Entities;
+
+namespace SynthShop.Data
 {
-    public class MainDbContext
+    public class MainDbContext : DbContext
     {
+        public MainDbContext(DbContextOptions<MainDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
