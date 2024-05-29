c21150e7693039bed48fca7f8c60a04b1a6ecda1
anuarsabitovich <anuarkulmagambetov@gmail.com>
2024-05-29 15:15:06 +0500

    Add UI temporary commit to save changes

---
+++ client/src/features/catalog/ProductDetails.tsx
@@ -0,0 +1,66 @@
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Product } from "../../app/models/product";
+import agent from "../../app/api/agent";
+import NotFound from "../../app/errors/NotFound";
+import LoadingComponent from "../../app/layout/LoadingComponent";
+
+type Params = {
+    id: string;
+};
+
+export default function ProductDetails() {
+    const { id } = useParams<Params>();
+    const [product, setProduct] = useState<Product | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        id && agent.Catalog.details(id)
+        .then(response => setProduct(response))
+        .catch(error => console.log(error.response))
+        .finally(() => setLoading(false))
+        
+    }, [id]);
+
+    if (loading) return <LoadingComponent message="Loading product..." /> ;
+    if (error) return <Typography>{error}</Typography>;
+    if (!product) return <NotFound/>
+
+    return (
+        <Grid container spacing={6}>
+            <Grid item xs={6}>
+                <img src={product.pictureUrl} alt={product.name} style={{ width: '100%' }} />
+            </Grid>
+            <Grid item xs={6}>
+                <Typography variant="h3">{product.name}</Typography>
+                <Divider/>
+                <Typography variant="h4" color='secondary' >{product.price}</Typography>
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{product.name}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Description</TableCell>
+                                <TableCell>{product.description}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Quantity in stock</TableCell>
+                                <TableCell>{product.stockQuantity}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+        </Grid>
+    )
+}
+
+function setError(arg0: string) {
+    throw new Error("Function not implemented.");
+}
