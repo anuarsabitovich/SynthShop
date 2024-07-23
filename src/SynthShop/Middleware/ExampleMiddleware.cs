5c8e7b4d2872395bf74515d18bdd4af257a1acba
anuarsabitovich <anuarkulmagambetov@gmail.com>
2024-07-23 16:55:11 +0500

    remove example middleware

--- src/SynthShop/Middleware/ExampleMiddleware.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SynthShop.Middleware;
-
-public class ExampleMiddleware : IMiddleware
-{
-    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
-    {
-        await next.Invoke(context);
-    }
-}
